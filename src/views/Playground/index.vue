0325e806390f216c4bfc8910c6a07afd02d21b6c
kkan0615 <kkan0615@gmail.com>
2021-04-15 23:34:31 +0900

    :rocket: version 0.1

---
+++ src/views/Playground/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    test:
+    {{ userInfo }}
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, onMounted, defineComponent } from 'vue'
+import useStore from '@/store'
+
+export default defineComponent({
+  name: 'Playground',
+  setup () {
+    const store = useStore()
+
+    const userInfo = computed(() => store.state.user)
+
+    onMounted(() => {
+      console.log(store)
+      console.log(userInfo)
+    })
+
+    return {
+      userInfo
+    }
+  }
+})
+</script>
